7cc33276cf50ba43cf10038864529b9ac76ff3e1
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-08-04 16:53:32 -0400

    Add functions for manipulating arbitrary base digital representations.

--- src/Math/Arithmetic/Digits.hs
+++ src/Math/Arithmetic/Digits.hs
@@ -1,7 +1,10 @@
 -- | Funtions for dealing with the digital representation of integers.
 module Math.Arithmetic.Digits (
     digits,
-    undigits
+    undigits,
+
+    digitsB,
+    undigitsB
 ) where
 
 import Data.Char (digitToInt)
@@ -13,3 +16,12 @@ digits = reverse . map (fromIntegral . digitToInt) . show
 -- | Recomposes a list of digits in base 10 to the corresponding integer.
 undigits :: [Integer] -> Integer
 undigits = sum . zipWith (*) (iterate (*10) 1)
+
+-- | Decomposes an integer into its representation in a given base, with least significant digit first.
+digitsB :: Integer -> Integer -> [Integer]
+digitsB _ 0 = []
+digitsB b n =  r : digitsB b q where (q, r) = n `quotRem` b
+
+-- | Recomposes a list of digits into an integer using the given base.
+undigitsB :: Integer -> [Integer] -> Integer
+undigitsB b = sum .zipWith (*) (iterate (*b) 1)
--- test/Math/Arithmetic/Digits/Test.hs
+++ test/Math/Arithmetic/Digits/Test.hs
@@ -10,9 +10,13 @@ import Math.Arithmetic.Digits
 prop_base10_roundtrip :: Integer -> Property
 prop_base10_roundtrip n = n >= 0 ==> (undigits $ digits n) == n
 
+prop_baseB_roundtrip :: Integer -> Integer -> Property
+prop_baseB_roundtrip b n = b > 1 && n >= 0 ==> (undigitsB b $ digitsB b n) == n
+
 tests :: [Test]
 tests = [
         testGroup "Digits" [
-                testProperty "Base 10 Roundtrip" prop_base10_roundtrip
+                testProperty "Base 10 Roundtrip" prop_base10_roundtrip,
+                testProperty "Base B Roundtrip" prop_baseB_roundtrip
             ]
     ]

c23fd45b7c13bd663573678fb5b88d64c0c6dff6
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-08-04 12:56:51 -0400

    Add a deep divisibility function.

--- src/Math/Arithmetic/Divisibility.hs
+++ src/Math/Arithmetic/Divisibility.hs
@@ -3,7 +3,8 @@ module Math.Arithmetic.Divisibility (
     (//),
     (%%),
 
-    (%?)
+    (%?),
+    (%!)
 ) where
 
 -- | An operator alias for 'div', for convenience.
@@ -17,3 +18,13 @@ module Math.Arithmetic.Divisibility (
 -- | A divisibility check; succeeds if the first integer precisely divides the second.
 (%?) :: Integer -> Integer -> Bool
 (%?) a b = b %% a == 0
+
+-- | A deep divisibility operator, where if @(s, t) = a %! b@, then @a^s * t == b@.
+(%!) :: Integer -> Integer -> (Integer, Integer)
+(%!) _ 0 = (1, 0)
+(%!) 1 b = (1, b)
+(%!) a b
+    | a %? b = (1 + s, t)
+    | otherwise = (0, b)
+  where
+    (s, t) = a %! (b // a)
--- test/Math/Arithmetic/Divisibility/Test.hs
+++ test/Math/Arithmetic/Divisibility/Test.hs
@@ -16,11 +16,21 @@ prop_check_oneDividesEverything x = 1 %? x
 prop_check_divisibilityOfMultiples :: Integer -> Integer -> Property
 prop_check_divisibilityOfMultiples x y = x /= 0 ==> x %? (x * y)
 
+prop_deep_identity :: Integer -> Property
+prop_deep_identity x = x > 0 ==> x %! x == (1, 1)
+
+prop_deep_invariant :: Integer -> Integer -> Property
+prop_deep_invariant x y = x > 0 && y >= 0 ==> x^s * t == y where (s, t) = x %! y
+
 tests :: [Test]
 tests = [
         testGroup "Divisibility Check" [
                 testProperty "Everything Divides Zero" prop_check_everythingDividesZero,
                 testProperty "One Divides Everything" prop_check_oneDividesEverything,
                 testProperty "Divisibility of Multiples" prop_check_divisibilityOfMultiples
+            ],
+        testGroup "Deep Divisibility" [
+                testProperty "Identity" prop_deep_identity,
+                testProperty "Invariant" prop_deep_invariant
             ]
     ]

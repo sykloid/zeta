9ea0cb5948293edde5beefa257d4e9231781d537
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-08-06 23:03:37 -0400

    Define a primality predicate, using prime trial division.

--- src/Math/Arithmetic/Primality.hs
+++ src/Math/Arithmetic/Primality.hs
@@ -1,9 +1,21 @@
 module Math.Arithmetic.Primality (
-    primes
+    primes,
+    isPrime
 ) where
 
 import qualified Data.Numbers.Primes as P
 
+import Math.Arithmetic.Divisibility
+
 -- | An infinite list of prime numbers.
 primes :: [Integer]
 primes = P.primes
+
+-- | Determine if the given integer is prime.
+isPrime :: Integer -> Bool
+isPrime n
+    | n <= 1 = False
+    | n == 2 = True
+    | otherwise = not . any (%? n) $ takeWhile (<=root) primes
+  where
+    root = ceiling . sqrt $ (fromIntegral n :: Double)

617f6fd79cdd61e76932528927a8db51439f418e
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-08-08 17:40:17 -0400

    Add tests for fibonacci sequence generation/lookup.

--- test/Math/Arithmetic/Test.hs
+++ test/Math/Arithmetic/Test.hs
@@ -5,10 +5,12 @@ import Test.Framework.Providers.API
 import qualified Math.Arithmetic.Digits.Test as Digits
 import qualified Math.Arithmetic.Divisibility.Test as Divisibility
 import qualified Math.Arithmetic.Primality.Test as Primality
+import qualified Math.Arithmetic.Sequences.Test as Sequences
 
 tests :: [Test]
 tests = [
         testGroup "Digits" Digits.tests,
         testGroup "Divisibility" Divisibility.tests,
-        testGroup "Primality" Primality.tests
+        testGroup "Primality" Primality.tests,
+        testGroup "Sequences" Sequences.tests
     ]
